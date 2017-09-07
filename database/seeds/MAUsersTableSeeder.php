a4904ff16815335f1a9aa9f337e27e82740f621e
don79is <don79is@gmail.com>
2017-09-07 11:59:30 +0300

    add user seed

---
+++ database/seeds/MAUsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MAUsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = array(
+            array
+            ('id' => 1,
+                'email' => 'doni@doni',
+                'first_name' => 'Donatas',
+                'last_name' => 'Tumanas',
+                'role_id' => 1,
+                'remember_token' => 'asdasd',
+                'position' => 'buhalteris',
+                'password' => Hash::make('demodemo'),
+            ),
+        );
+        DB::table('ma_users')->insert($data);
+    }
+}
