598cd389fd294623563ad91ae906b5e80a7d7727
don79is <don79is@gmail.com>
2017-09-08 09:52:34 +0300

    add bind

--- app/Http/Middleware/Corse.php
+++ app/Http/Middleware/Corse.php
@@ -16,7 +16,7 @@ class Corse
     public function handle($request, Closure $next)
     {
         return $next($request)->header('Access-Control-Allow-Origin', '*')
-            ->header('Accses-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS')
+            ->header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS')
 //             ->header("Access-Control-Allow-Origin", "*");
             ->header("Access-Control-Allow-Credentials", "true")
             ->header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
